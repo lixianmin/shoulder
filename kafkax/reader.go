e33575c26e8233982994bd45ef0696122e2fa08a
lixianmin <lixianmin@live.cn>
2021-03-09 12:12:30 +0800

    封装kafka.Reader

--- kafkax/reader.go
+++ kafkax/reader.go
@@ -1,6 +1,7 @@
 package kafkax
 
 import (
+	"context"
 	"github.com/segmentio/kafka-go"
 	"os"
 	"path/filepath"
@@ -13,7 +14,12 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
-func NewReader(brokers []string, topic string, options ...ReaderOption) *kafka.Reader {
+type Reader struct {
+	reader      *kafka.Reader
+	uncommitted []kafka.Message
+}
+
+func NewReader(brokers []string, topic string, options ...ReaderOption) *Reader {
 	// 我们还是很希望一眼能够看到默认值的
 	var serviceName = filepath.Base(os.Args[0])
 	var args = readerArguments{
@@ -36,5 +42,42 @@ func NewReader(brokers []string, topic string, options ...ReaderOption) *kafka.R
 		Logger:   &Logger{},
 	})
 
-	return reader
+	var my = &Reader{
+		reader:      reader,
+		uncommitted: make([]kafka.Message, 0, 16),
+	}
+
+	return my
+}
+
+func (my *Reader) FetchMessage(ctx context.Context) (kafka.Message, error) {
+	var msg, err = my.reader.FetchMessage(ctx)
+	if err != nil {
+		return msg, err
+	}
+
+	my.uncommitted = append(my.uncommitted, msg)
+	return msg, nil
+}
+
+func (my *Reader) CommitMessages(ctx context.Context) error {
+	var count = len(my.uncommitted)
+	if count == 0 {
+		return nil
+	}
+
+	if err := my.reader.CommitMessages(ctx, my.uncommitted...); err != nil {
+		return err
+	}
+
+	my.uncommitted = my.uncommitted[:0]
+	return nil
+}
+
+func (my *Reader) GetUncommittedCount() int {
+	return len(my.uncommitted)
+}
+
+func (my *Reader) GetReader() *kafka.Reader {
+	return my.reader
 }
