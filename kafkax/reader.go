04758a2347f17879372cf829489c3446e95fe361
lixianmin <lixianmin@live.cn>
2021-03-25 13:14:05 +0800

    fix

--- kafkax/reader.go
+++ kafkax/reader.go
@@ -126,11 +126,11 @@ func (my *Reader) Offset() int64 {
 }
 
 func (my *Reader) setReader(reader *kafka.Reader) {
-	atomic.StorePointer((*unsafe.Pointer)(my.reader), unsafe.Pointer(reader))
+	atomic.StorePointer((*unsafe.Pointer)(&my.reader), unsafe.Pointer(reader))
 }
 
 func (my *Reader) Reader() *kafka.Reader {
-	var p = (*kafka.Reader)(atomic.LoadPointer((*unsafe.Pointer)(my.reader)))
+	var p = (*kafka.Reader)(atomic.LoadPointer((*unsafe.Pointer)(&my.reader)))
 	return p
 }
 
