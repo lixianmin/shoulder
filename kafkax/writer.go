6b49986e980d8fbffceedf4edefdb4c69bf3846a
lixianmin <lixianmin@live.cn>
2021-04-15 15:44:33 +0800

    comment

--- kafkax/writer.go
+++ kafkax/writer.go
@@ -15,6 +15,7 @@ Copyright (C) - All Rights Reserved
 
 func NewWriter(brokers []string, topic string, options ...WriterOption) *kafka.Writer {
 	// 我们还是很希望一眼能够看到默认值的
+	// http://www.mobabel.net/%E6%80%BB%E7%BB%93kafka%E6%80%A7%E8%83%BD%E8%B0%83%E4%BC%98%E5%92%8C%E5%8F%82%E6%95%B0%E8%B0%83%E4%BC%98/
 	var args = writerArguments{
 		balancer:     &kafka.LeastBytes{},
 		requiredAcks: kafka.RequireAll,      // 默认值RequireAll，等待所有ISR成员的ack之后再返回Write()方法
