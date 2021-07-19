d06f835adcd7247bbfbac1442a7ddf6bda8a2caa
lixianmin01 <lixianmin01@baidu.com>
2021-07-19 18:10:44 +0800

    有的时候processed不大于0，很奇怪，但不知道为啥

--- kafkax/reader_lag_monitor.go
+++ kafkax/reader_lag_monitor.go
@@ -86,6 +86,8 @@ func (my *readerLagMonitor) calculateEstimateTime(lasting time.Duration, lagNum 
 	var processed = my.startLagNum - lagNum
 	if processed > 0 {
 		estimateTime = lasting * time.Duration(lagNum/processed)
+	} else {
+		my.startLagNum = lagNum // 有的时候processed不大于0，很奇怪，但不知道为啥
 	}
 
 	return estimateTime
