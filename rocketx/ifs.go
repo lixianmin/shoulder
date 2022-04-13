bc09b05270a807fa7e16d0fea7dd1ace2da269c0
lixianmin01 <lixianmin01@xmli.local>
2022-04-13 10:37:01 +0800

    删除Message接口的中GetUniqueKey() string方法

--- rocketx/ifs.go
+++ rocketx/ifs.go
@@ -9,5 +9,4 @@ Copyright (C) - All Rights Reserved
 
 type Message interface {
 	GetShardingKey() string // 当使用uid作为sharding key的时候, 同一个用户的消息将发送到同一个queue中, 这样在该用户身上可做到顺序一致性. 如果一直返回空串, 则会fallback到random路由
-	GetUniqueId() int64     // 用于保证end to end一致性, 幂等性保证
 }
